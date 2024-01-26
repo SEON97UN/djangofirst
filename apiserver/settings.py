af81bb9bd6d958e50f9fd9b26cd774ae943ce3d8
seon97un <tjdwns9703@gmail.com>
2024-01-26 12:05:45 +0900

    메시지

--- apiserver/settings.py
+++ apiserver/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-96v+t-d)e675s1mn1h2pp!(mvlsm$rapli_u_wo#x+x^fc_y4u
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = []
 
 
 # Application definition
